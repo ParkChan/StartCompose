2fade93b83e71822fe8b968cfd40d05ada38671d
iron883 <hydrarkk@naver.com>
2023-03-09 01:37:55 +0900

    [추가] 레이아웃 구성

--- app/src/main/java/com/chan/startcompose/MainActivity.kt
+++ app/src/main/java/com/chan/startcompose/MainActivity.kt
@@ -4,16 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.chan.startcompose.ui.theme.StartComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,11 +41,17 @@ fun MessageCard(msg: Message) {
     Row {
         Image(
             painter = painterResource(R.drawable.profile_picture),
-            contentDescription = "Contact profile picture"
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
         )
+
+        Spacer(modifier = Modifier.width(8.dp))
 
         Column {
             Text(text = msg.author)
+            Spacer(modifier = Modifier.height(4.dp))
             Text(text = msg.body)
         }
 
