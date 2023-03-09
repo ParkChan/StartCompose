a09acd1f5d3c5a99d8ddf31d559918bab17bf283
iron883 <hydrarkk@naver.com>
2023-03-10 01:53:02 +0900

    [추가] 어두운 테마 사용

--- app/src/main/java/com/chan/startcompose/MainActivity.kt
+++ app/src/main/java/com/chan/startcompose/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.chan.startcompose
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -73,7 +74,12 @@ fun MessageCard(msg: Message) {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun DefaultPreview() {
     ComposeTutorialTheme {
